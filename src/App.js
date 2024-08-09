e575f23cbb03b889b0afeeb0dd71bf2085bbd179
Lucas Bailo <lucasebailo@gmail.com>
2024-08-09 10:29:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
 
 function App() {
+
+  const labelRef = useRef('')
+  const inputRef = useRef('')
+
+  useEffect(() => {
+    labelRef.current.innerHTML = "USER NAME"
+    labelRef.current.style.fontStyle = 'italic'
+  }, [])
+
+  const sendForm = (event) => {
+    event.preventDefault()
+    // send data...
+    inputRef.current.focus()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={sendForm}>
+        <div>
+          <label ref={labelRef}>Name</label>
+        </div>
+        <div>
+          <input placeholder='Type your name...' ref={inputRef} />
+        </div>
+        <div>
+          <label>Email</label>
+        </div>
+        <div>
+          <input placeholder='Type your email...' type='email' />
+        </div>
+        <div>
+          <button>Save</button>
+        </div>
+      </form>
     </div>
   );
 }
